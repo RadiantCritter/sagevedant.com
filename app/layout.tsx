13a2ed35c83add4323570cab6a27adedbab093ae
RadiantCritter <156023946+RadiantCritter@users.noreply.github.com>
2024-12-10 19:50:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'sagevedant',
+    template: '%s | sagevedant',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'sagevedant',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'sagevedant',
     locale: 'en_US',
     type: 'website',
   },
